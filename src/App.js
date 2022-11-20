bea5056da6b0d1ccf508e30c4b4f108b29b23782
JaeiKim <dobbykim0320@gmail.com>
2022-11-21 04:33:39 +0900

    게시판1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
+
+axios.defaults.withCredentials = true;
+
+function Join() {
+  const navigation = useNavigate();
+
+  const [data, setData] = React.useState({
+    id: "",
+    pw: "",
+  });
+
+  const 데이터변경 = (event) => {
+    const name = event.target.name;
+    const cloneData = { ...data };
+    cloneData[name] = event.target.value;
+    setData(cloneData);
+  };
+
+  const 회원가입 = async () => {
+    await axios({
+      url: "http://localhost:4000/join",
+      method: "POST",
+      data: data,
+    })
+      .then((res) => {
+        const { code, message } = res.data;
+        if (code === "success") {
+          alert(message);
+          navigation("/login");
+        }
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log("join Error", e);
+      });
+  };
+
+  return (
+    <div>
+      <input type="text" name="id" onChange={데이터변경} />
+      <input type="password" name="pw" onChange={데이터변경} />
+      <button type="button" onClick={회원가입}>
+        회원가입
+      </button>
+    </div>
+  );
+}
+
+function Login() {
+  const [data, setData] = React.useState({
+    id: "",
+    pw: "",
+  });
+
+  const 데이터변경 = (event) => {
+    const name = event.target.name;
+    const cloneData = { ...data };
+    cloneData[name] = event.target.value;
+    setData(cloneData);
+  };
+
+  const 로그인 = async () => {
+    await axios({
+      url: "http://localhost:4000/login",
+      method: "POST",
+      data: data,
+    })
+      .then((res) => {})
+      .catch((e) => {
+        console.log("로그인에러", e);
+      });
+  };
+
+  return (
+    <div>
+      <input name="id" onChange={데이터변경} />
+      <input name="pw" onChange={데이터변경} />
+      <button type="button" onClick={로그인}>
+        로그인
+      </button>
+    </div>
+  );
+}
+
+function Main() {
+  return <div>Main</div>;
+}
+
+const StoreContext = React.createContext({});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreContext.Provider value={{}}>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/join" element={<Join />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </StoreContext.Provider>
   );
 }
 
